6f0cfdbd0f2099aa9d65b1926fec81126c19bd37
Sarah Kagia <kagiasarah22@gmail.com>
2025-07-29 18:07:40 +0300

    first commit

---
+++ assignment1.py
@@ -0,0 +1,32 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+operation = input("Enter an operation(+, -, *, /): ")
+
+if operation =="+":
+    result = num1 + num2
+    print("Result:", result)
+    
+elif operation == "-":
+    result = num1 - num2
+    print("Result:", result)
+    
+elif operation == "*":
+    result = num1 * num2
+    print("Result:", result)
+elif operation == "/":
+    if num2 !=0:
+      result = num1/num2
+      print("Result:", result)  
+    else:
+        print("Error: Cannot divide by zero")
+else:
+        print("Invalid operation selected")
+      
+    
+    
+    
+    
+    
+    
+    
